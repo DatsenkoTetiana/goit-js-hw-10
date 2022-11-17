5c523bf73a6559e054ae94fae62e3f2aaaf20aeb
tetianadac <datsenkotetiana027@gmail.com>
2022-11-17 13:03:48 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './css/styles.css';
+import { Notify } from 'notiflix';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+const searchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
